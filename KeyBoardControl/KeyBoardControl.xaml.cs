18875133befec52e3e3b7ee283d660b31bfd5b22
Alex Horton <alex.j.horton@yahoo.co.uk>
2011-04-22 22:05:08 +0100

    Initial Keyboard Control WPF

---
+++ KeyBoardControl/KeyBoardControl.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyBoardControlLibrary
+{
+    /// <summary>
+    /// Interaction logic for KeyBoardControl.xaml
+    /// </summary>
+    public partial class KeyBoardControl : UserControl
+    {
+        private int numberOfOctaves = 8;
+
+        public KeyBoardControl(int numberOfOctaves)
+        {
+            this.numberOfOctaves = numberOfOctaves;
+            Initialise();
+        }
+        
+        public KeyBoardControl()
+        {
+            Initialise();
+        }
+
+        private void Initialise()
+        {
+            InitializeComponent();
+            Visibility = System.Windows.Visibility.Visible;
+            AddKeys();
+        }
+
+        public void AddKeys()
+        {
+
+            for (int i = 1; i <= numberOfOctaves; i++) DrawOctave(i);
+        }
+
+        private void DrawOctave(int octaveNumber)
+        {
+            double OctaveOffset = (PianoKey.WhiteKeyWidth / 2) + ((octaveNumber - 1) * 7 * PianoKey.WhiteKeyWidth);
+            var keys = new List<PianoKey> 
+            { 
+                new PianoKey(KeyTypes.White, (octaveNumber + 1) * 12 + 0, OctaveOffset + PianoKey.WhiteKeyWidth * 0.0),
+                new PianoKey(KeyTypes.White, (octaveNumber + 1) * 12 + 2, OctaveOffset + PianoKey.WhiteKeyWidth * 1.0),
+                new PianoKey(KeyTypes.White, (octaveNumber + 1) * 12 + 4, OctaveOffset + PianoKey.WhiteKeyWidth * 2.0),
+                new PianoKey(KeyTypes.White, (octaveNumber + 1) * 12 + 5, OctaveOffset + PianoKey.WhiteKeyWidth * 3.0),
+                new PianoKey(KeyTypes.White, (octaveNumber + 1) * 12 + 7, OctaveOffset + PianoKey.WhiteKeyWidth * 4.0),
+                new PianoKey(KeyTypes.White, (octaveNumber + 1) * 12 + 9, OctaveOffset + PianoKey.WhiteKeyWidth * 5.0),
+                new PianoKey(KeyTypes.White, (octaveNumber + 1) * 12 + 11, OctaveOffset + PianoKey.WhiteKeyWidth * 6.0),
+                new PianoKey(KeyTypes.Black, (octaveNumber + 1) * 12 + 1, OctaveOffset + PianoKey.WhiteKeyWidth * 0.5),
+                new PianoKey(KeyTypes.Black, (octaveNumber + 1) * 12 + 3, OctaveOffset + PianoKey.WhiteKeyWidth * 1.5),
+                new PianoKey(KeyTypes.Black, (octaveNumber + 1) * 12 + 6, OctaveOffset + PianoKey.WhiteKeyWidth * 3.5),
+                new PianoKey(KeyTypes.Black, (octaveNumber + 1) * 12 + 8, OctaveOffset + PianoKey.WhiteKeyWidth * 4.5),
+                new PianoKey(KeyTypes.Black, (octaveNumber + 1) * 12 + 10, OctaveOffset + PianoKey.WhiteKeyWidth * 5.5)
+            };
+
+            foreach (var key in keys)
+            {
+                //MessageBox.Show(key.ToString());
+                KeyBoardCanvas.Children.Add(key);
+            }
+        }
+    }
+}
---
+++ KeyBoardControl/Piano/PianoKey.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows;
+
+namespace KeyBoardControlLibrary
+{
+    public class PianoKey : Shape
+    {
+        
+        #region ctor and member variable declarations
+
+        public static int WhiteKeyWidth = 10;
+        public static int KeyOffset = WhiteKeyWidth / 2;
+        public static int BlackKeyWidth = 5;
+        public static int WhiteKeyHeight = 30;
+        public static int BlackKeyHeight = WhiteKeyHeight * 2 / 3;
+
+        public KeyTypes KeyType { get; set; }
+        public int KeyMidiId { get; private set; }
+        private double centreXPosition;
+
+
+        public PianoKey(KeyTypes keyType, int keyMidiId, double centrePosition)
+        {
+            KeyType = keyType;
+            KeyMidiId = keyMidiId;
+            centreXPosition = centrePosition;
+
+            Stroke = Brushes.Black;
+            SetDefaultKeyColour();
+            
+            
+            MouseEnter += (o, e) =>
+                {
+                    Fill = Brushes.GreenYellow;
+                };
+            MouseLeave += (o, e) =>
+                {
+                    SetDefaultKeyColour();
+                };
+            MouseUp += (o, e) => MessageBox.Show("This key had Midi id: " + KeyMidiId);
+            
+        }
+        #endregion
+
+        #region implemented abstract methods from base class
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                Rect rect;
+                if (KeyType == KeyTypes.Black)
+                {
+                    rect = new Rect(centreXPosition - BlackKeyWidth / 2.0, 0, BlackKeyWidth, BlackKeyHeight);
+                }
+                else 
+                {
+                    rect = new Rect(centreXPosition - WhiteKeyWidth / 2.0, 0, WhiteKeyWidth, WhiteKeyHeight);
+                }
+                return new RectangleGeometry(rect);                
+            }
+        }
+        #endregion
+
+        #region Events
+        
+        #endregion Events
+
+        private void SetDefaultKeyColour()
+        {
+            Fill = (KeyType == KeyTypes.White) ? Brushes.Ivory : Brushes.Black;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("Fill: {0}, Stroke: {1}, ActualHeight{2}, ActualWidth{3}", Fill, Stroke, ActualHeight, ActualWidth);
+        }
+    }
+
+    public enum KeyTypes { White, Black };
+}
